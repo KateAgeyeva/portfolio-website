616b8f31ad446e385576131a56b616c305342566
Kate Ageyeva <kate.ag.au@gmail.com>
2022-03-29 14:08:02 +0200

    About me info text changes

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,4 +1,3 @@
-import myImage from '../assets/_MG_3229.jpeg';
 import styles from './AboutMe.module.css';
 import Paragraph from './Paragraph';
 import Paragr from '../models/paragr';
@@ -6,7 +5,7 @@ import HeaderMenu from './HeaderMenu';
 
 function AboutMe() {
     const paragraphs = [
-        new Paragr('About me', 'I have been learning Front-end development for a year starting with HTML/CSS till React/React Native. I am fascinated by React development and everything connected to it and currently open for Junior/Entry-level positions. I went through many courses (html/css, D3.js, JavaScript, React, React Native) as long as my own projects. I worked on a project based on Agile development process with 2 more people. Previously I worked for an IT company and familiar with software development processes.'),
+        new Paragr('About me', 'I have been learning Front-end development for a year starting with HTML/CSS till React/React Native. I am fascinated by React development and everything connected to it and currently open for Junior/Entry-level positions. I went through many courses (html/css, D3.js, JavaScript, React, React Native) as long as my own projects. I worked on a project based on Agile development process with 2 more people. Previously I studed software QA and worked for an IT company. That is why I am familiar with software development processes.'),
         new Paragr('Skills', 'HTML, CSS, Javascript, React, React Native (Expo CLI), Redux/Redux Toolkit, Bootstrap, Tailwind, D3.JS, Node.JS + Express, REST API, Next.JS, Jest (basic), TypeScript (basic), SQL(basic), Git, Jira, Heroku, Vercel, Android Studio, VSCode.')
     ];
     return (
