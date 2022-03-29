5a5cd649e43c2c62e585c5220b2aa2b76df349a5
Kate Ageyeva <kate.ag.au@gmail.com>
2022-03-29 14:04:31 +0200

    UI/GUI adjustments + new design news-rate app screenshots

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -6,8 +6,8 @@ import HeaderMenu from './HeaderMenu';
 
 function AboutMe() {
     const paragraphs = [
-        new Paragr('About me', 'I have been learning Front-end development for almost a year starting with HTML/CSS till React/React Native. I am fascinated by React development and everything connected to it. Open for Junior/Entry-level positions. I went through many courses (html/css, d3.js, JavaScript, React, React Native) along with my own projects. I worked on a project based on Agile development process with 2 more people. Previously I worked for an IT company and familiar with software development processes.'),
-        new Paragr('Skills', 'HTML, CSS, Javascript, React, React Native (Expo CLI), Redux, Bootstrap, Tailwind, D3.JS, Node.JS + Express, REST API, Next.JS, Jest (basic), TypeScript (basic), SQL(basic),  Git, Jira, Heroku, Vercel, Android Studio, VSCode.')
+        new Paragr('About me', 'I have been learning Front-end development for a year starting with HTML/CSS till React/React Native. I am fascinated by React development and everything connected to it and currently open for Junior/Entry-level positions. I went through many courses (html/css, D3.js, JavaScript, React, React Native) as long as my own projects. I worked on a project based on Agile development process with 2 more people. Previously I worked for an IT company and familiar with software development processes.'),
+        new Paragr('Skills', 'HTML, CSS, Javascript, React, React Native (Expo CLI), Redux/Redux Toolkit, Bootstrap, Tailwind, D3.JS, Node.JS + Express, REST API, Next.JS, Jest (basic), TypeScript (basic), SQL(basic), Git, Jira, Heroku, Vercel, Android Studio, VSCode.')
     ];
     return (
       <div className={styles.container}>
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -26,11 +26,9 @@ const Projects = () => {
     return (
       <div className={styles.containerproj} id='projects'>
         <div className={styles.projone}>
-          <p className={styles.header}>News Rate Web App</p>
+          <p className={styles.header}>News Rates Web App</p>
           <p className={styles.text}>
-            This project is based on React/Next.js. Here you can find top-20
-            latest news in the Netherlands and compare any news worldwide
-            filling a form. <a href="https://github.com/KateAgeyeva/news-rate" target="_blank">GitHub: KateAgeyeva/news-rate</a>
+            Here you can find top-20 latest news in the Netherlands and compare any news popularity worldwide filling a form. This website can help journalists, researchers and other people. The project is based on React/Next.js. <a href="https://github.com/KateAgeyeva/news-rate" target="_blank">GitHub: KateAgeyeva/news-rate</a>
           </p>
           <div className={styles.examples}>
               <a href={image} target="_blank">
@@ -45,27 +43,31 @@ const Projects = () => {
         <div className={styles.projtwo}>
           <p className={styles.header}>Writer Helper Mobile App</p>
           <p className={styles.text}>
-            The project is based on React Native/Expo CLI. Here you can creat/edit your books, add chapters and notes. <a href="https://github.com/KateAgeyeva/writerhelper" target="_blank">GitHub: KateAgeyeva/writerhelper</a>
+            Here you can creat/edit books, add chapters and notes, share the books and chapters in DOCX format. The app has a simple design to let writers concentrating on a content of their books as it could be on a plain paper sheet. The project is based on React Native/Expo CLI. <a href="https://github.com/KateAgeyeva/writerhelper" target="_blank">GitHub: KateAgeyeva/writerhelper</a>
           </p>
           <div className={styles.imgsecondcontainer}>
-              <a href={im1} target="_blank">
-                <img className={styles.imgsecond} src={im1}></img>
-              </a>
-              <a href={im2} target="_blank">
-                <img className={styles.imgsecond} src={im2}></img>
-              </a>
-              <a href={im3} target="_blank">
-                <img className={styles.imgsecond} src={im3}></img>
-              </a>
-              <a href={im4} target="_blank">
-                <img className={styles.imgsecond} src={im4}></img>
-              </a>
-              <a href={im5} target="_blank">
-                <img className={styles.imgsecond} src={im5}></img>
-              </a>
-              <a href={im6} target="_blank">
-                <img className={styles.imgsecond} src={im6}></img>
-              </a>
+              <div>
+                <a href={im1} target="_blank">
+                  <img className={styles.imgsecond} src={im1}></img>
+                </a>
+                <a href={im2} target="_blank">
+                  <img className={styles.imgsecond} src={im2}></img>
+                </a>
+                <a href={im3} target="_blank">
+                  <img className={styles.imgsecond} src={im3}></img>
+                </a>
+              </div>
+              <div>
+                <a href={im4} target="_blank">
+                  <img className={styles.imgsecond} src={im4}></img>
+                </a>
+                <a href={im5} target="_blank">
+                  <img className={styles.imgsecond} src={im5}></img>
+                </a>
+                <a href={im6} target="_blank">
+                  <img className={styles.imgsecond} src={im6}></img>
+                </a>
+              </div>
           </div>
         </div>
       </div>
