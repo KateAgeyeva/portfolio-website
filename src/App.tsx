6d573252c96b5a59881593629963db94e125b091
Kate Ageyeva <kate.ag.au@gmail.com>
2022-04-11 11:22:54 +0200

    mobile responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,15 @@
 import AboutMe from './components/AboutMe';
 import Contacts from './components/Contacts';
 import Projects from './components/Projects';
+import { Fragment } from 'react';
 
 function App() {
   return (
-    <div>
+    <Fragment>
       <AboutMe />
       <Projects />
       <Contacts />
-    </div>
+    </Fragment>
   );
 }
 
